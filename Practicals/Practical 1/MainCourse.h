4c7486edba821896c808599e4f0efeeb638d6863
u22658395 <u22658395@tuks.co.za>
2024-07-26 22:12:35 +0200

    Implemented the specification file of the MainCourse class

--- Practicals/Practical 1/MainCourse.h
+++ Practicals/Practical 1/MainCourse.h
@@ -1,1 +1,13 @@
 
+#ifndef MAINCOURSE_H
+#define MAINCOURSE_H
+
+#include "Course.h"
+
+class Dessert{
+    public:
+        Dessert(int maxNumberOfItems);
+        virtual void recommendBeverage();
+        // virtual ~Dessert();
+};
+#endif
