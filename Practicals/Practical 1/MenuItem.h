7a2d0bdd53e271b2e8312dbee87d416c05aaa0b1
u22658395 <u22658395@tuks.co.za>
2024-07-26 21:40:46 +0200

    Implemented the specification file for the MenuItem class

--- Practicals/Practical 1/MenuItem.h
+++ Practicals/Practical 1/MenuItem.h
@@ -1,1 +1,19 @@
+#ifndef MENUITEM_H
+#define MENUITEM_H
 
+#include<iostream>
+using namespace std;
+class MenuItem{
+    public:
+        MenuItem(string description, float price, int stock);
+        string getDescription();
+        float getPrice();
+        int getStock();
+        void reduceStock();
+
+    private:
+        string description;
+        float price;
+        int stock;
+};
+#endif
