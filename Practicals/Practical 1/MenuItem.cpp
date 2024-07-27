356bcb2a6ba3ead976a0d4094db813839f908b20
u22658395 <u22658395@tuks.co.za>
2024-07-27 12:02:48 +0200

    Implemented the functions of MenuItem

--- Practicals/Practical 1/MenuItem.cpp
+++ Practicals/Practical 1/MenuItem.cpp
@@ -1,1 +1,23 @@
+#include "MenuItem.h"
 
+MenuItem::MenuItem(string description, float price, int stock){
+    this->description = description;
+    this->price = price;
+    this->stock = stock;
+}
+
+string MenuItem::getDescription(){
+    return this->description;
+}
+
+float MenuItem::getPrice(){
+    return this->price;
+}
+
+int MenuItem::getStock(){
+    return this->stock;
+}
+
+void MenuItem::reduceStock(){
+    --stock; 
+}
