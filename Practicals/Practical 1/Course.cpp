beea2a8bce6449c62c45591ca92c194e58fdff36
u22658395 <u22658395@tuks.co.za>
2024-07-27 14:29:44 +0200

    Implemented the methods of the Course class

---
+++ Practicals/Practical 1/Course.cpp
@@ -0,0 +1,56 @@
+#include "Course.h"
+
+Course::Course(string description, int maxNumberOfItems){
+    this->description = description;
+    this->maxNumberOfItems = maxNumberOfItems;
+}
+
+string Course::getDescription(){
+    return description;
+}
+
+bool Course::addMenuItem(string description, float price, int stock){
+    int iniSize = menuItems.size();
+    menuItems.push_back(new MenuItem(description,price, stock));
+    return iniSize < menuItems.size();
+}
+
+void Course::printMenuItems(){
+    char charIndex = 'a';
+
+    for (int i = 0; i < menuItems.size(); i++,charIndex++)
+    {
+        cout << '\t' << charIndex << ".\t" << menuItems.at(i)->getDescription() <<'\n';
+    }
+}
+
+void Course::printInventory(){
+    char charIndex = 'a';
+    for (int i = 0; i < menuItems.size(); i++,charIndex++)
+    {
+        cout << '\t' << charIndex << ".\t" << menuItems.at(i)->getDescription() <<'\t' << menuItems.at(i)->getPrice() <<'\t'<< menuItems.at(i)->getStock() <<'\n';
+    }
+}
+
+MenuItem* Course:: getMenuItem(int index){
+    try
+    {
+        return menuItems.at(index);
+    }
+    catch(const std::out_of_range& e)
+    {
+        return nullptr;
+    }
+    
+}
+
+Course::~Course(){
+    for (int i = 0; i < menuItems.size(); i++)
+    {
+        delete menuItems.at(i);
+        menuItems.at(i) = nullptr;
+    }
+
+    menuItems.clear();
+    
+}
