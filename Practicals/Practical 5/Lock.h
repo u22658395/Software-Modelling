da3315ea40abd85e629768743b6e19782c34f8bf
u22658395 <u22658395@tuks.co.za>
2024-10-06 16:03:58 +0200

    Implemented the Lock class

---
+++ Practicals/Practical 5/Lock.h
@@ -0,0 +1,23 @@
+#ifndef Lock_H
+#define Lock_H
+#include "Device.h"
+
+class Lock : public Device
+{
+    private:
+        /* data */
+        bool locked;
+    public:
+        Lock();
+        Lock(bool state);
+        virtual ~Lock();
+        virtual string getStatus();
+        virtual void toggleState();
+        virtual string getDeviceType();
+        virtual void turnOn();
+        virtual void turnOff();
+        void lock();
+        void unlock();
+        void toggleLock();
+};
+#endif
