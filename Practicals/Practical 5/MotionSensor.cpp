891cc01fef79ccc8878b7f87eba6bbe3da77f770
boitymtsatse <boitymtsatse@gmail.com>
2024-10-07 10:52:33 +0200

    implemented removeDevices and notifyDevices for observer pattern

--- Practicals/Practical 5/MotionSensor.cpp
+++ Practicals/Practical 5/MotionSensor.cpp
@@ -1,6 +1,7 @@
 #include "MotionSensor.h"
+#include <iostream>
 
-MotionSensor::MotionSensor(/* args */)
+MotionSensor:: MotionSensor(/* args */)
 {
 
 }
@@ -10,9 +11,28 @@ MotionSensor:: ~MotionSensor()
 }
 void MotionSensor:: addDevice(Observer* device)
 {
-
+    devices.push_back(device);
 }
 void MotionSensor:: removeDevice(Observer* device)
+{
+    for (auto i = devices.begin(); i != devices.end(); ++i)
+    {
+        if(*i==device)
+        {
+            devices.erase(i);
+            break;
+        }
+    }
+}
 
-void notifyDevices(Observer* device);
-void detectMotion();
+void MotionSensor:: notifyDevices(Observer* device)
+{
+        for (const auto& device : devices) {
+            device->update();
+        }
+}
+void MotionSensor:: detectMotion()
+{
+    std::cout << "Motion detected!\n";
+        notifyDevices("MotionDetected");
+}
--- Practicals/Practical 5/Sensor.h
+++ Practicals/Practical 5/Sensor.h
@@ -6,10 +6,10 @@
 class Sensor {
 
 protected:
-    std:: vector<Observer> devices;
+    std:: vector<Observer*> devices;
 public:
-    virtual void addDevice(std::shared_ptr<Observer> device) = 0;
-    virtual void removeDevice(std::shared_ptr<Observer> device) = 0;
+    virtual void addDevice(Observer* device) = 0;
+    virtual void removeDevice(Observer* device) = 0;
     virtual void notifyDevices(const std::string& event) = 0;
     virtual ~Sensor() {}
 };
