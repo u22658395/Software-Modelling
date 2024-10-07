0610ba61d7857ec3d517b4cecc0351ad69bd5c12
u22658395 <u22658395@tuks.co.za>
2024-10-07 22:26:38 +0200

    Implemented the getStatus method in the Room class

--- Practicals/Practical 5/Room.h
+++ Practicals/Practical 5/Room.h
@@ -2,6 +2,7 @@
 #define ROOM_H
 #include "Command.h"
 #include "vector"
+#include "light.h"
 
 //this is the Invoker in the Command pattern
 class Room : public Device
@@ -10,11 +11,11 @@ class Room : public Device
         /* data */
         Command* lightCommand;
         Command* thermostatCommand;
-        vector<Device> devices;
+        vector<Device*> devices;
         bool on;
 
     public:
-        Room(/* args */);
+        Room(string t);
         ~Room();        
         void setLightCommand(Command* command);        
         void setThermostatCommand(Command* command);
@@ -23,5 +24,6 @@ class Room : public Device
         virtual string getStatus();
         virtual void toggleState();
         virtual void performAction(bool state);
+        void addDevice(Device* d);
 };
 #endif
